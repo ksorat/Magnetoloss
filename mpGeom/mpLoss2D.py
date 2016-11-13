269a2e5c4c779fbe5958c7424a0a348fcbb40a69
Kareem <kareem.sorathia@gmail.com>
2016-11-13 11:41:09 -0700

    Fast push

--- mpGeom/mpLoss2D.py
+++ mpGeom/mpLoss2D.py
@@ -10,7 +10,7 @@ import matplotlib.gridspec as gridspec
 #Figure defaults
 figSize = (8,8)
 figQ = 300 #DPI
-#figQ = 50
+
 
 figStub = "mpLoss"
 doFirst = False
@@ -40,8 +40,10 @@ P0 = -120; P1 = 120
 L0 = -60; L1 = 60
 pBin = np.linspace(P0,P1,Np)
 lBin = np.linspace(L0,L1,Nl)
+gs = gridspec.GridSpec(2,1,height_ratios=[1,3])
 
 for i in range(Ns):
+	fig = plt.figure()
 	fIn = RootDir + spcs[i] + "." + fileStub
 	figName = figStub + "." + spcs[i] + ".png"
 	print("Generating %s"%figName)
@@ -49,22 +51,27 @@ for i in range(Ns):
 		R,Phi,Lambda,Tl = lfmpp.getSphLoss1st(fIn)
 	else:
 		R,Phi,Lambda = lfmpp.getSphLoss(fIn)
+
 	#fig = plt.figure(figsize=figSize,tight_layout=True)
-	fig = plt.figure()
-	gs = gridspec.GridSpec(2,1,height_ratios=[1,3])
-	plt.axis('scaled')
+	
+	
+	
 	
 	#1D histogram
 	Ax1D = fig.add_subplot(gs[0,0])
 	Ax1D.hist(Phi,pBin,color='blue',normed=True)
-	Ax1D.set_xlim(P0,P1)
-	plt.setp(Ax1D.get_xticklabels(),visible=False)
 	
 
 	#2D histogram
 	Ax2D = fig.add_subplot(gs[1,0])
 	Ax2D.hist2d(Phi,Lambda,[pBin,lBin],cmap=cMap,normed=True,norm=LogNorm(vmin=cAx[0],vmax=cAx[1]) )
-	Ax2D.set_xlim(P0,P1)
+
+	#Axes
+	# plt.axis('scaled')
+	# Ax1D.set_xlim(P0,P1)
+	# plt.setp(Ax1D.get_xticklabels(),visible=False)
+
+	# Ax2D.set_xlim(P0,P1)
 	#Ax2D.set_ylim(L0,L1)
 	#Ax2D.set_xticks(xTk)
 	#Ax2D.set_xticklabels(xTkLab)
