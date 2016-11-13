bd7ca4bfcaec58338c7ab54461da64b730a37e13
Kareem <kareem.sorathia@gmail.com>
2016-11-12 19:33:32 -0700

    Fast push

---
+++ gen/lossT.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+import lfmViz as lfmv
+import lfmPostproc as lfmpp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+dirStub = "/Users/soratka1/Work/magnetoloss/synth"
+RootDir = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/"
+fileStub = "100keV.h5part"
+
+spcs = ["H","O","e"]
+Leg = ["H+","O+","e-"]
+
+Ns = len(spcs)
+#fig = plt.figure(1, figsize=(10, 10))
+
+Np = 100000.0
+for i in range(Ns):
+	fIn = RootDir + spcs[i] + "." + fileStub
+	t,mpTp = lfmpp.getH5p(fIn,"mp")
+	mpT = mpTp.sum(axis=1)
+	print(mpT.shape)
+	plt.plot(t,mpT/Np)
+
+plt.legend(Leg,loc='lower right')
+plt.xlabel('Time [s]'); plt.ylabel('Cumulative Loss Fraction')
