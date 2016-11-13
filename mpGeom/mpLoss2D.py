62f8df0c0798e1a4c9a0938a608f58f2e99f2ebd
Kareem <kareem.sorathia@gmail.com>
2016-11-12 20:43:56 -0700

    Fast push

--- mpGeom/mpLoss2D.py
+++ mpGeom/mpLoss2D.py
@@ -17,6 +17,10 @@ cMap="jet"
 Np = 50
 Nl = 50
 
+xTk = [-90,-45,0,45,90]
+xTkLab = ["Dawn","9:00","Noon","15:00","Dusk"]
+
+
 RootDir = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/"
 fileStub = "100keV.h5part"
 spcs = ["H","O","e"]
@@ -30,9 +34,10 @@ else:
 	Ns = len(spcs)
 
 lfmv.initLatex()
-
-pBin = np.linspace(-180,180,Np)
-lBin = np.linspace(-60,60,Nl)
+P0 = -180; P1 = 180
+L0 = -60; L1 = 60
+pBin = np.linspace(P0,P1,Np)
+lBin = np.linspace(L0,L1,Nl)
 
 for i in range(Ns):
 	fIn = RootDir + spcs[i] + "." + fileStub
@@ -42,14 +47,23 @@ for i in range(Ns):
 	else:
 		R,Phi,Lambda = lfmpp.getSphLoss(fIn)
 	fig = plt.figure(figsize=figSize,tight_layout=True)
-	gs = gridspec.GridSpec(2,1,height_ratios=[1,1])
+	plt.axis('scaled')
+	gs = gridspec.GridSpec(2,1,height_ratios=[1,4])
 
 	#1D histogram
 	Ax1D = fig.add_subplot(gs[0,0])
 	Ax1D.hist(Phi,pBin,color='blue',normed=True)
-
+	plt.setp(Ax.get_xticklabels(),visible=False)
+	plt.xlim(P0,P1)
+	
 	#2D histogram
 	Ax2D = fig.add_subplot(gs[1,0])
 	Ax2D.hist2d(Phi,Lambda,[pBin,lBin],cmap=cMap,normed=True,norm=LogNorm(vmin=cAx[0],vmax=cAx[1]) )
+	plt.xlim(P0,P1)
+	plt.ylim(L0,L1)
+	Ax2D.set_xticks(xTk)
+	Ax2D.set_xticklabels(xTkLab)
+
+
 	plt.savefig(figName,dpi=figQ)
 	plt.close('all')
