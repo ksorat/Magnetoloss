e65b89d1f879a735dfc17e08b2ce6b793ec0c3de
ksorat <kareem.sorathia@gmail.com>
2016-12-30 11:02:55 -0500

    Fast push

--- trj3d/gen3D.py
+++ trj3d/gen3D.py
@@ -13,11 +13,13 @@ import lfmPostproc as lfmpp
 Np = 100000
 cIDs = [1335,301,95834,12593,63464,75685]
 h5id = "O.100keV.h5part"
-fGen = True
+fGen = False
 
 
 aIDs = np.arange(1,Np+1)
-fIn = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/" + h5id
+rootDir = os.path.expanduser('~') + "/Work/Magnetoloss/Data/"
+fIn = rootdir+ "H5p/" + h5id
+
 fOut = "o3d.h5p"
 if (fGen):
 	Mask = np.zeros(Np,dtype=bool)
@@ -27,3 +29,16 @@ if (fGen):
 	lfmpp.subH5p(fIn,Mask,len(cIDs),fOut=fOut)
 
 #Now do visit stuff
+Src0 = "/glade/u/home/skareem/scratch/lfmVTIs/sns/SNS-Bz-5-Vx400-N5-F200_mhd_1200000.vti"
+
+if (Quiet):
+        LaunchNowin()
+else:
+        Launch()
+
+#Do some defaults
+pyv.lfmExprsEB()
+pyv.pvInit()
+
+OpenDatabase(Src0)
+OpenDatabase(fIn)
