a2c9605d417f9ccd52052d587002edcc83d91eeb
ksorat <kareem.sorathia@gmail.com>
2017-01-05 16:06:33 -0500

    Fast push

--- fldTrc/lamCrit.py
+++ fldTrc/lamCrit.py
@@ -27,7 +27,13 @@ xTk = [-135,-90,-45,0,45,90,135]
 
 pSlcs = [0,30,45,60,75,90]
 
-fIn = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/O.100keV.h5part"
+fIn1 = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/H.100keV.h5part"
+fIn2 = os.path.expanduser('~') + "/Work/Magnetoloss/Data/H5p/O.100keV.h5part"
+fIns = [fIn1,fIn2]
+
+Spcs = ["H+","O+"]
+Cols = ["r","b","g","c","k","m"]
+Mrks = ["-","--"]
 
 lfmv.ppInit()
 P0 = -150; P1 = 150
@@ -37,20 +43,24 @@ pC = 0.5*(pBin[0:-1] + pBin[1:])
 lBin = np.linspace(L0,L1,Nl)
 lC = 0.5*(lBin[0:-1] + lBin[1:])
 
-R,Phi,Lambda = lfmpp.getSphLoss(fIn)
-X,pI,lI = np.histogram2d(Phi,Lambda,[pBin,lBin])
+NumSpc = len(Spcs)
 
-Xs = ndimage.filters.gaussian_filter(X,1,mode='nearest')
+for n in range(NumSpc):
+	R,Phi,Lambda = lfmpp.getSphLoss(fIns[n])
+	X,pI,lI = np.histogram2d(Phi,Lambda,[pBin,lBin])
 
-nS = len(pSlcs)
-for i in range(nS):
-	pS = pSlcs[i]
-	dP = np.abs(pC-pS)
-	I = np.abs(pC-pS).argmin()
-	labS = "Phi = %d"%(pS)
-	plt.plot(lC,Xs[I,:],label=labS)
-	Ic = Xs[I,:].argmax()
-	print("@ Phi = %d, LamC = %f"%(pS,lC[Ic]))
+	Xs = ndimage.filters.gaussian_filter(X,1,mode='nearest')
+	print("Species %s"%(Spcs[n]))
+	nS = len(pSlcs)
+	for i in range(nS):
+		pS = pSlcs[i]
+		dP = np.abs(pC-pS)
+		I = np.abs(pC-pS).argmin()
+		pStr = "%s%s"%(Cols[i],Mrks[n])
+		labS = "Phi = %d (%s)"%(pS,Spcs[n])
+		plt.plot(lC,Xs[I,:],pStr,label=labS)
+		Ic = Xs[I,:].argmax()
+		print("\t@ Phi = %d, LamC = %f"%(pS,lC[Ic]))
 
 plt.legend()
 plt.savefig("lamCrit.png")
