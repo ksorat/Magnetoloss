eaeae89f946fd3be96d670cf3e727ca6ab60351c
ksorat <kareem.sorathia@gmail.com>
2017-02-08 10:04:37 -0500

    Fast push

--- post/mltKA.py
+++ post/mltKA.py
@@ -11,19 +11,37 @@ import cPickle as pickle
 def getXKA(fIn):
 	isOut = lfmpp.getOut(fIn,mp=True)
 
+	#Get spherical coordinates of last MP Xing
+	R,Phi,Lambda = lfmpp.getSphLoss(fIn)
+
 	#Get time index of last MP crossing
-	t,tMP = lfmpp.getH5p(fIn,"tCr")
-	tMP = tMP[:,isOut]
+	t,tMP = lfmpp.getH5p(fIn,"tCr",Mask=isOut)
 
 	tsMP = np.argmax(tMP,axis=0)
 	#Use these slices to find values at last X'ing
-	t,Kt = lfmpp.getH5p(fIn,"kev")
-	Kt = Kt[:,isOut]
+	t,Kt  = lfmpp.getH5p(fIn,"kev",Mask=isOut)
+	t,Vxt = lfmpp.getH5p(fIn,"vx",Mask=isOut)
+	t,Vyt = lfmpp.getH5p(fIn,"vy",Mask=isOut)
+	t,Vzt = lfmpp.getH5p(fIn,"vz",Mask=isOut)
 
-	mlt = tsMP
-	K = 0
-	A = 0
-	return mlt,Kt,A
+
+	Np = Kt.shape[1]
+	K = np.zeros(Np)
+	A = np.zeros(Np)
+
+	for i in range(Np):
+		tsi = tsMP[i]
+		vxi = Vxt[tsi,i]
+		vyi = Vyt[tsi,i]
+		vzi = Vzt[tsi,i]
+		vmag = np.sqrt(vxi**2.0+vyi**2.0+vzi**2.0)
+
+		K[i] = Kt[tsi,i]
+		A[i] = np.arccos(vzi/vmag)*180/np.pi
+
+	mlt = Phi
+	return mlt,K,A
+
 figSize = (8,8)
 figQ = 300 #DPI
 
@@ -50,8 +68,7 @@ if (os.path.isfile(msDataFile)):
 else:
 	print("No data file found, calculating")
 
-	#for i in range(Ns):
-	for i in [1]:	
+	for i in range(Ns):
 		fIn = RootDir + spcs[i] + "." + fileStub
 		print("Reading %s"%(fIn))
 
