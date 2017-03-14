770d31f9c114610c340ee4278f5f87dbac83f9e8
ksorat <kareem.sorathia@gmail.com>
2017-03-14 18:05:04 -0400

    Fast push

--- sheath/msFig.py
+++ sheath/msFig.py
@@ -31,12 +31,12 @@ ppi = np.arctan2(yyi,xxi)
 #Find MLT slice 15:00
 pMLT = 45.0
 pi0 = (180/np.pi)*ppi[:,0]
-pc0 = 0.5*(pi0[0:-2] + pi0[1:-1])
+pc0 = 0.5*(pi0[0:-1] + pi0[1:])
 
 
 mltJ = np.abs(pc0-pMLT).argmin()
 Rci = rri[mltJ,:]
-Rc = 0.5*(Rci[0:-2] + Rci[1:-1])
+Rc = 0.5*(Rci[0:-1] + Rci[1:])
 BxCC,ByCC,BzCC = lfm.getHDFVec(hdffile,'b')
 
 Bx0 = bScl*BxCC[ks,mltJ,:]
