b734f1b2fd4285d2253cac543d31e880d02c98df
ksorat <kareem.sorathia@gmail.com>
2017-03-14 17:56:06 -0400

    Fast push

--- sheath/msFig.py
+++ sheath/msFig.py
@@ -13,6 +13,7 @@ import lfmInterp as lfm
 fIn = "/glade/p/hao/wiltbemj/SNS/ION/SNS-Bz-5-Vx400-N5-F200/SNS-Bz-5-Vx400-N5-F200_mhd_2000000.hdf"
 Re = 6.38e+8 #Earth radius [cm]
 iRe = 1/Re
+bScl = 1.0e+5 #Gauss->nT
 
 hdffile = SD(fIn)
 #Grab x/y/z arrays from HDF file.  Scale by Re
@@ -31,11 +32,15 @@ ppi = np.arctan2(yyi,xxi)
 pMLT = 45.0
 pi0 = (180/np.pi)*ppi[:,0]
 pc0 = 0.5*(pi0[0:-2] + pi0[1:-1])
-mltJ = np.abs(pc0-pMLT).argmin()
 
+
+mltJ = np.abs(pc0-pMLT).argmin()
+Rci = rri[mltJ,:]
+Rc = 0.5*(Rci[0:-2] + Rci[1:-1])
 BxCC,ByCC,BzCC = lfm.getHDFVec(hdffile,'b')
 
-Bx0 = BxCC[ks,mltJ,:]
-By0 = BxCC[ks,mltJ,:]
-Bz0 = BxCC[ks,mltJ,:]
+Bx0 = bScl*BxCC[ks,mltJ,:]
+By0 = bScl*BxCC[ks,mltJ,:]
+Bz0 = bScl*BxCC[ks,mltJ,:]
 
+print("Field maxes = ", np.abs(Bx0).max(),np.abs(By0).max(),np.abs(Bz0).max())
