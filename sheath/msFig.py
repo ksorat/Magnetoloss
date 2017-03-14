0d66649aeff50873c28785a19b0ca2eb9916a2da
ksorat <kareem.sorathia@gmail.com>
2017-03-14 17:37:33 -0400

    Fast push

--- sheath/msFig.py
+++ sheath/msFig.py
@@ -12,7 +12,7 @@ from pyhdf.SD import SD, SDC
 fIn = "/glade/p/hao/wiltbemj/SNS/ION/SNS-Bz-5-Vx400-N5-F200/SNS-Bz-5-Vx400-N5-F200_mhd_2000000.hdf"
 Re = 6.38e+8 #Earth radius [cm]
 
-hdffile = SD(fname)
+hdffile = SD(fIn)
 #Grab x/y/z arrays from HDF file.  Scale by Re
 x3 = iRe*np.double(hdffile.select('X_grid').get())
 y3 = iRe*np.double(hdffile.select('Y_grid').get())
@@ -22,7 +22,7 @@ ks = 0 #Upper half x-y plane
 xxi = x3[ks,:,:].squeeze()
 yyi = y3[ks,:,:].squeeze()
 rri = np.sqrt(xxi**2.0 + yyi**2.0)
-lfmg.ppi = np.arctan2(yyi,xxi)
+ppi = np.arctan2(yyi,xxi)
 
 
 
